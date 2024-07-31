238b0d91f9f03727111a3e66986beddb49fd2fc9
ashrafbel <ashrafmus2@gmail.com>
2024-07-31 14:42:59 -0700

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,18 +6,22 @@ from models import storage
 from flask import Flask, render_template
 from models.state import State
 
+
 app = Flask(__name__)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
     """Close the storage"""
     storage.close()
 
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """list of all State objects in pages html"""
     s = storage.all(State).values()
     return render_template('7-states_list.html', states=s)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
